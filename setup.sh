2a216b4fd936a9119d6f447a3d6e134e63f5cb74
clomie <cclomie@gmail.com>
2018-05-28 09:46:17 +0900

    Add gitconfig

--- setup.sh
+++ setup.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-DOT_FILES=( .zshrc .zshrc.tmux .tmux.conf .tmux.conf.osx .tmux.conf.others .vimrc )
+DOT_FILES=( .zshrc .zshrc.tmux .tmux.conf .tmux.conf.osx .tmux.conf.others .gitconfig .vimrc )
 
 for file in ${DOT_FILES[@]}
 do
     ln -s $HOME/.dotfiles/$file $HOME/$file
 done
 
+case ${OSTYPE} in
+  cygwin*|msys*)
+    ln -s $HOME/.dotfiles/.gitconfig.win $HOME/.gitconfig.os
+    ;;
+esac
+
 mkdir -p $HOME/.vim/undo $HOME/.vim/tmp
+
