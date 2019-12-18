c104399f56bab52b93169c050fd7ac011aa1e3ae
clomie <cclomie@gmail.com>
2019-12-18 18:21:34 +0900

    シンボリックリンクを上書き可能にした

--- setup.sh
+++ setup.sh
@@ -6,15 +6,16 @@ DOT_FILES=( .zshrc .zshrc.tmux .tmux.conf .tmux.conf.osx .tmux.conf.others .gitc
 
 for file in ${DOT_FILES[@]}
 do
-    ln -s $CURRENTDIR/$file $HOME/$file
+    ln -sf $CURRENTDIR/$file $HOME/$file
 done
 
 case ${OSTYPE} in
   cygwin*|msys*)
-    ln -s $HOME/.dotfiles/.gitconfig.win $HOME/.gitconfig.os
+    ln -sf $HOME/.dotfiles/.gitconfig.win $HOME/.gitconfig.os
     ;;
   darwin*)
-    ln -s $CURRENTDIR/Brewfile $HOME/Brewfile
+    ln -sf $CURRENTDIR/Brewfile $HOME/Brewfile
+    ;;
 esac
 
 mkdir -p $HOME/.vim/undo $HOME/.vim/tmp
